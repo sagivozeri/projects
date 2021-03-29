870691b5981fa9fef92c713668e61270bcbf9567
Sagiv Ozeri <sozeri@habana.ai>
2021-03-29 10:52:00 +0300

    modify project 27
    fix primes.c
    
    Signed-off-by: Sagiv Ozeri <sozeri@habana.ai>

--- proj_27/primes.c
+++ proj_27/primes.c
@@ -31,8 +31,8 @@ int main(int argc, char **argv) {
 
         int n = 10; //default to the first 10 primes
         
-        if (argc = 2) {
-                atoi(argv[2]);
+        if (argc == 2) {
+                n = atoi(argv[1]);
         }
         
         int *primes = getPrimes(n);
@@ -41,31 +41,35 @@ int main(int argc, char **argv) {
         
         printf("The sum of the first %d primes is %d\n", n, s);
 
+	free(primes);
+
         return 0;
 }
 
 int sum(int *arr, int n) {
         int i;
-        int total;
+        int total = 0;
         
         for (i = 0 ; i < n ; i++) {
-                total =+ arr[i];
+                total += arr[i];
         }
         
         return total;
 }
 
 int* getPrimes(int n) {
-        int result[n];
-        int i = 0;
-        int x = 2;
+        int *result = malloc(sizeof(int) * n);
+        int i = 1;
+        int x = 3;
+	
+	result[0] = 2;
 
         while (i < n) {
                 if (isPrime(x)) {
                         result[i] = x;
                         i++;
-                        x += 2;
                 }
+                x += 2;
         }
         
         return result;
@@ -76,9 +80,7 @@ int isPrime(int x) {
 
 	if (x == 2) return 1;
 
-        if (x % 2 == 0) {
-                return 0;
-        }
+        if (x % 2 == 0) return 0;
         
         for (int i = 3 ; i <= sqrt(x) ; i+=2) {
                 if(x % i == 0) {
