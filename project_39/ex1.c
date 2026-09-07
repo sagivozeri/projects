134d99a968cab3ef7bcaf685aa0c3c41f3fa09b8
Sagiv Ozeri <sagiv.ozeri@elmnt.com>
2026-09-07 15:31:59 +0300

    fix project 39
    Signed-off-by: Sagiv Ozeri <sagiv.ozeri@elmnt.com>

--- project_39/ex1.c
+++ project_39/ex1.c
@@ -1,72 +1,72 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _list {
-	int val;
-	struct _list *next;
+  int val;
+  struct _list *next;
 } list;
 
-static int delete_nth_elem(list **head, int n, int *val)
-{
-	list *tmp, **curr = head;
+static int delete_nth_elem(list **head, int n, int *val) {
+  list *tmp, **curr = head;
 
-	if (!head || !(*head) || n <= 0) {
-		printf("Invalid input\n"); 
-		return -1;
-	}
+  if (!head || !(*head) || n <= 0 || !val) {
+    printf("Invalid input\n");
+    return -1;
+  }
 
-	printf("curr = %p\n", curr);
+  printf("curr = %p\n", curr);
 
-	for (int i = 1; i < n; i++) {
-		curr = &((*curr)->next);
-		printf("curr = %p\n", curr);
-		if (!(*curr)) {
-			printf("No %d elements in the list\n", n);
-			return -1;
-		}
- 	}
+  for (int i = 1; i < n; i++) {
+    curr = &((*curr)->next);
+    printf("curr = %p\n", curr);
+    if (!(*curr)) {
+      printf("No %d elements in the list\n", n);
+      return -1;
+    }
+  }
 
-	tmp = *curr;
-	*curr = (*curr)->next;
-	*val = tmp->val;
+  tmp = *curr;
+  *curr = (*curr)->next;
+  *val = tmp->val;
 
-	return 0;
+  return 0;
 }
 
-void print_list(list *head)
-{
-	printf("List: ");
-	while (head) {
-		printf("%d ", head->val);
-		head = head->next;
-	}
-	printf("\n");
+void print_list(list *head) {
+  printf("List: ");
+  while (head) {
+    printf("%d ", head->val);
+    head = head->next;
+  }
+  printf("\n");
 }
 
 int main() {
-	list x, y, z, *head;
-	int rc, val;
+  list x, y, z, *head;
+  int rc, val;
 
-	head = &x;
-	x.val = 1;
-	x.next = &y;
-	y.val = 2;
-	y.next = &z;
-	z.val = 3;
-	z.next = NULL;	
+  head = &x;
+  x.val = 1;
+  x.next = &y;
+  y.val = 2;
+  y.next = &z;
+  z.val = 3;
+  z.next = NULL;
 
-	printf("head %p x.next %p y.next %p z.next %p\n", head, x.next, y.next, z.next);
-	printf("&head %p &x.next %p &y.next %p &z.next %p\n", &head, &(x.next), &(y.next), &(z.next));
+  printf("head %p x.next %p y.next %p z.next %p\n", head, x.next, y.next,
+         z.next);
+  printf("&head %p &x.next %p &y.next %p &z.next %p\n", &head, &(x.next),
+         &(y.next), &(z.next));
 
-	print_list(head);
+  print_list(head);
 
-	rc = delete_nth_elem(&head, 3, &val);
-	if (rc)
-		return 1;
+  rc = delete_nth_elem(&head, 3, &val);
+  if (rc)
+    return 1;
 
-	printf("val = %d\n", val);
+  printf("val = %d\n", val);
 
-	print_list(head);
-	
-	return 0;
+  print_list(head);
+
+  return 0;
 }
